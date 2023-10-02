8561cee03f75b262a9e82de2d0867264f1cb159e
samayounis <your@email.here>
2023-10-02 17:44:15 -0400

    updated admin feature

--- pages/admins.js
+++ pages/admins.js
@@ -1,9 +1,63 @@
 import Layout from "@/components/Layout";
-import { useState } from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {withSwal} from "react-sweetalert2";
+import Spinner from "@/components/Spinner";
+import {prettyDate} from "@/lib/date";
 
-export default function AdminsPage() {
+function AdminsPage({swal}) {
   const [email,setEmail] = useState('');
-  function addAdmin(){}
+  const [adminEmails,setAdminEmails] = useState([]);
+  const [isLoading,setIsLoading] = useState(false);
+  function addAdmin(ev){
+    ev.preventDefault();
+    axios.post('/api/admins', {email}).then(res => {
+      console.log(res.data);
+      swal.fire({
+        title: 'Admin created!',
+        icon: 'success',
+      });
+      setEmail('');
+      loadAdmins();
+    }).catch(err => {
+      swal.fire({
+        title: 'Error!',
+        text: err.response.data.message,
+        icon: 'error',
+      });
+    });
+  }
+  function deleteAdmin(_id, email) {
+    swal.fire({
+      title: 'Are you sure?',
+      text: `Do you want to delete admin ${email}?`,
+      showCancelButton: true,
+      cancelButtonText: 'Cancel',
+      confirmButtonText: 'Yes, Delete!',
+      confirmButtonColor: '#d55',
+      reverseButtons: true,
+    }).then(async result => {
+      if (result.isConfirmed) {
+        axios.delete('/api/admins?_id='+_id).then(() => {
+          swal.fire({
+            title: 'Admin deleted!',
+            icon: 'success',
+          });
+          loadAdmins();
+        });
+      }
+    });
+  }
+  function loadAdmins() {
+    setIsLoading(true);
+    axios.get('/api/admins').then(res => {
+      setAdminEmails(res.data);
+      setIsLoading(false);
+    });
+  }
+  useEffect(() => {
+    loadAdmins();
+  }, []);
   return (
     <Layout>
       <h1>Admins</h1>
@@ -34,11 +88,33 @@ export default function AdminsPage() {
           </tr>
         </thead>
         <tbody>
+          {isLoading && (
             <tr>
-              <td>test@gamil.com</td>
+              <td colSpan={2}>
+                <div className="py-4">
+                  <Spinner fullWidth={true} />
+                </div>
+              </td>
             </tr>
+          )}
+          {adminEmails.length > 0 && adminEmails.map(adminEmail => (
+            <tr>
+              <td>{adminEmail.email}</td>
+              <td>
+                {adminEmail.createdAt && prettyDate(adminEmail.createdAt)}
+              </td>
+              <td>
+                <button
+                  onClick={() => deleteAdmin(adminEmail._id, adminEmail.email)} className="btn-red">Delete</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Layout>
   );
 }
+
+export default withSwal(({swal}) => (
+  <AdminsPage swal={swal} />
+));
