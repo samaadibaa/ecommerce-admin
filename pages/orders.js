6c659f56d49996f1ad605f2a217d0be510da9e54
samayounis <your@email.here>
2023-10-03 10:00:38 -0400

    key props added to orders

--- pages/orders.js
+++ pages/orders.js
@@ -1,11 +1,12 @@
 import Layout from "@/components/Layout";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Spinner from "@/components/Spinner";
 
 export default function OrdersPage() {
-  const [orders,setOrders] = useState([]);
-  const [isLoading,setIsLoading] = useState(false);
+  const [orders, setOrders] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     setIsLoading(true);
     axios.get('/api/orders').then(response => {
@@ -13,6 +14,7 @@ export default function OrdersPage() {
       setIsLoading(false);
     });
   }, []);
+
   return (
     <Layout>
       <h1>Orders</h1>
@@ -26,37 +28,35 @@ export default function OrdersPage() {
           </tr>
         </thead>
         <tbody>
-        {isLoading && (
-          <tr>
-            <td colSpan={4}>
-              <div className="py-4">
-                <Spinner fullWidth={true} />
-              </div>
-            </td>
-          </tr>
-        )}
-        {orders.length > 0 && orders.map(order => (
-          <tr>
-            <td>{(new Date(order.createdAt)).toLocaleString()}
-            </td>
-            <td className={order.paid ? 'text-green-600' : 'text-red-600'}>
-              {order.paid ? 'YES' : 'NO'}
-            </td>
-            <td>
-              {order.name} {order.email}<br />
-              {order.city} {order.postalCode} {order.country}<br />
-              {order.streetAddress}
-            </td>
-            <td>
-              {order.line_items.map(l => (
-                <>
-                  {l.price_data?.product_data.name} x
-                  {l.quantity}<br />
-                </>
-              ))}
-            </td>
-          </tr>
-        ))}
+          {isLoading && (
+            <tr key="loading">
+              <td colSpan={4}>
+                <div className="py-4">
+                  <Spinner fullWidth={true} />
+                </div>
+              </td>
+            </tr>
+          )}
+          {orders.length > 0 && orders.map(order => (
+            <tr key={order._id}>
+              <td>{(new Date(order.createdAt)).toLocaleString()}</td>
+              <td className={order.paid ? 'text-green-600' : 'text-red-600'}>
+                {order.paid ? 'YES' : 'NO'}
+              </td>
+              <td>
+                {order.name} {order.email}<br />
+                {order.city} {order.postalCode} {order.country}<br />
+                {order.streetAddress}
+              </td>
+              <td>
+                {order.line_items.map((l, index) => (
+                  <div key={index}>
+                    {l.price_data?.product_data.name} x {l.quantity}<br />
+                  </div>
+                ))}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Layout>
